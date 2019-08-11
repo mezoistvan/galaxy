2706a3e9b1d8e658cdc6b686d14c18be959ebc13
mezoistvan <mezoistvan1@gmail.com>
2019-08-11 13:17:41 +0200

    Push to github

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# galaxy
+# Solar system
+
+A real-time solar system illustration for @adam.krasz's popup exhibition in Budapest, 16-19 August 2019.
---
+++ script.js
@@ -0,0 +1,57 @@
+var initialDate = new Date('2019-08-13 20:00');
+var initialRotations = {
+    mercury: 55,
+    venus: 310,
+    earth: 130,
+    mars: 299,
+    jupiter: 185,
+    saturn: 163,
+    uranus: 57,
+    neptune: 104
+}; // in degrees
+
+var orbitTimes = {
+    mercury: 7603200,
+    venus: 19414080,
+    earth: 31558153,
+    mars: 59356800,
+    jupiter: 374335776,
+    saturn: 929577600,
+    uranus: 2651486400,
+    neptune: 5199724800
+}; // in seconds
+
+var currentDate = new Date();
+var secondsSinceInitialDate = (currentDate.getTime() - initialDate.getTime()) / 1000;
+function getCurrentRotationForPlanet(planetName) {
+    return (secondsSinceInitialDate / orbitTimes[planetName]) * 360 + initialRotations[planetName];
+}
+
+Object.keys(initialRotations).forEach(function (planetName) {
+    var currentRotation = getCurrentRotationForPlanet(planetName);
+    var el = document.querySelector('.' + planetName);
+    el.style.transform = 'rotate(' + currentRotation + 'deg)';
+    el.classList.add('anim')
+});
+
+function toHHMMSS(date) {
+    var hours   = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
+
+setInterval(function() {
+    var date = new Date();
+    document.querySelector('.time span').innerHTML = toHHMMSS(date);
+}, 500);
+
+setInterval(function() {
+    var date = new Date();
+    document.querySelector('.title span').innerHTML = date.toLocaleDateString();
+}, 500);
